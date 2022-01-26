028db271b74f2c5ed12fc64d24e9536928b47f6f
Rishi Srivastava <rishisrivasta@gmail.com>
2022-01-27 04:38:26 +0800

    async example

--- threads/src/main/java/com/concurrency/async/trigger/AsyncExample.java
+++ threads/src/main/java/com/concurrency/async/trigger/AsyncExample.java
@@ -19,16 +19,22 @@ public class AsyncExample {
 
         ExecutorService executor1 = Executors.newSingleThreadExecutor();
 //        ExecutorService executor2 = Executors.newSingleThreadExecutor();
+        ExecutorService executor2 = Executors.newFixedThreadPool(2);
 
         Supplier<List<Long>> supplyIDs = () -> {
             sleep(200);
             return Arrays.asList(1L, 2L, 3L);
         };
 
-        Function<List<Long>, List<User>> fetchUsers = ids -> {
+        Function<List<Long>, CompletableFuture<List<User>>> fetchUsers = ids -> {
             sleep(300);
             System.out.println("Function is running in " + Thread.currentThread().getName());
-            return ids.stream().map(User::new).collect(Collectors.toList());
+            Supplier<List<User>> userSupplier = () -> {
+                System.out.println("Supplier is running in " + Thread.currentThread().getName());
+                return ids.stream().map(User::new).collect(Collectors.toList());
+            };
+//            return CompletableFuture.supplyAsync(userSupplier);
+            return CompletableFuture.supplyAsync(userSupplier, executor2);
         };
 
         Consumer<List<User>> displayer = users -> {
@@ -37,36 +43,15 @@ public class AsyncExample {
         };
 
         CompletableFuture<List<Long>> completableFuture = CompletableFuture.supplyAsync(supplyIDs);
-        completableFuture.thenApply(fetchUsers)
+        //completableFuture.thenApply(fetchUsers)
+//        completableFuture.thenCompose(fetchUsers)
+        completableFuture.thenComposeAsync(fetchUsers, executor2)
 //                .thenAccept(displayer);
                 .thenAcceptAsync(displayer, executor1);
 
-        sleep(1_000);
+        sleep(1_000); // without sleep, the code will not print anything
         executor1.shutdown();
-
-        /*Function<List<Long>, CompletableFuture<List<User>>> fetchUsers = ids -> {
-            sleep(300);
-            System.out.println("Function is currently running in " + Thread.currentThread().getName());
-            Supplier<List<User>> userSupplier =
-                    () -> {
-                        System.out.println("Currently running in " + Thread.currentThread().getName());
-                        return ids.stream().map(User::new).collect(Collectors.toList());
-                    };
-            return CompletableFuture.supplyAsync(userSupplier, executor2);
-        };
-
-        Consumer<List<User>> displayer = users -> {
-            System.out.println("Running in " + Thread.currentThread().getName());
-            users.forEach(System.out::println);
-        };
-
-        CompletableFuture<List<Long>> completableFuture = CompletableFuture.supplyAsync(supplyIDs);
-        completableFuture.thenComposeAsync(fetchUsers, executor2)
-                .thenAcceptAsync(displayer, executor1);
-
-        sleep(1_000);
-        executor1.shutdown();
-        executor2.shutdown();*/
+        executor2.shutdown();
     }
 
     private static void sleep(int timeout) {
